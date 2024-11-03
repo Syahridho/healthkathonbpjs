12d669dca6bcc16a280f2d9f128a0c4d81e5b578
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-11-03 12:39:11 +0700

    add payment

--- script.js
+++ script.js
@@ -18,36 +18,47 @@ $(document).ready(function () {
     return $state;
   }
 
-    // Tab switching functionality
-    $('.btn-active, .btn-no-active').click(function () {
-      // Remove active class from all buttons
-      $('.btn-active, .btn-no-active').removeClass('btn-active').addClass('btn-no-active');
-      
-      // Add active class to the clicked button
-      $(this).removeClass('btn-no-active').addClass('btn-active');
-  
-      // Hide all tab contents
-      $('.tab-pane').removeClass('show active');
-  
-      // Show the relevant tab content based on which button was clicked
-      if ($(this).text().trim() === 'Tagihan') {
-        $('#v-pills-tagihan').addClass('show active');
-      } else if ($(this).text().trim() === 'Riwayat') {
-        $('#v-pills-riwayat').addClass('show active');
-      }
-    });
-  
+  // Tab switching functionality
+  $(".btn-active, .btn-no-active").click(function () {
+    // Remove active class from all buttons
+    $(".btn-active, .btn-no-active")
+      .removeClass("btn-active")
+      .addClass("btn-no-active");
+
+    // Add active class to the clicked button
+    $(this).removeClass("btn-no-active").addClass("btn-active");
+
+    // Hide all tab contents
+    $(".tab-pane").removeClass("show active");
+
+    // Show the relevant tab content based on which button was clicked
+    if ($(this).text().trim() === "Tagihan") {
+      $("#v-pills-tagihan").addClass("show active");
+    } else if ($(this).text().trim() === "Riwayat") {
+      $("#v-pills-riwayat").addClass("show active");
+    }
+  });
 });
 
+let nomor = document.getElementById("nomor");
+let isi = document.getElementById("isi");
 
-let nomor = document.getElementById('nomor');
-let isi = document.getElementById('isi');
-
-
-nomor.addEventListener("change", function() {
+nomor.addEventListener("change", function () {
   if (nomor.value === "nik") {
-    isi.innerHTML = "NIK"
+    isi.innerHTML = "NIK";
   } else {
-    isi.innerHTML = "NO BPJS"
+    isi.innerHTML = "NO BPJS";
   }
-})
+});
+
+function ubahStatus(btn) {
+  // Mengubah warna button menjadi hijau dengan mengganti class
+  btn.classList.remove("btn-secondary");
+  btn.classList.add("btn-success");
+
+  // Mengubah text button
+  btn.textContent = "Berhasil Dibayar";
+
+  // Opsional: menonaktifkan button setelah diklik
+  btn.disabled = true;
+}
